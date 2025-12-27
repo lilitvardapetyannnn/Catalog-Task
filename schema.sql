86a9aabd0f848d6071e6af4d2660e042b155f0bd
lilitvardapetyannnn <lilit.vardapetyan@edu.ysu.am>
2025-12-27 17:20:29 +0400

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS catalog_db;
+USE catalog_db;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    image VARCHAR(255)
+);
